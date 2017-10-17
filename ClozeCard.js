d21288d63c7f48f14dd8eb4bf64951d2799a8d3b
Kim Mathis <quenbess@gmail.com>
2017-10-17 00:33:34 -0500

    working copy?

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,31 +1,22 @@
 // The cloze can be found in front of the sentence, at the end of the sentence 
-// or in the middle of the sentence.  The array resulting from the split will consist
-// of two entries. If the cloze is at the front of the sentence, the length of the 
-// the entry at index 0 will be 0. If the cloze is at the end of the sentence the length of
-// the entry at index 1 will be 0. If the cloze is in the middle, the length of each of the
-// two entries will be greater than 0.
-
 function ClozeCard(text, cloze) {
-   if (this instanceof ClozeCard){
+   if (this instanceof ClozeCard){       //if this function was called using the "new" keyword
 	this.text = text;
 	this.cloze = cloze;
-	var result = text.split(cloze);
 	this.partial = "";
 
-	if (result[0].length === 0) {
-		this.partial = "..." + result[1];
-		} else if (result[1].length === 0) {	          
-			this.partial = result[0] + "...";
-			} else {
-				this.partial = result[0] + "..." 
-				+ result[1];
-			}	
-
-//	console.log("result is " + partial);
+	if (this.text.toLowerCase().includes(this.cloze.toLowerCase())){            //"includes" is case-sensitive
+		this.partial = this.text.replace(cloze, '...');                                  // replace the cloze portion with "..."
 	} else {
-		return new ClozeCard(text, cloze);
+		return console.log("Text to delete, " + this.cloze + ", must appear in the full text, " + this.text);
 	}
-}
+
+   } else {
+		return new ClozeCard(text, cloze);    // this is executed if the constructor was not called using the "new" keyword
+   }
+};
+
 module.exports = ClozeCard;
+
 //var nc = new ClozeCard("G. W. was the first prez of the US.", "G. W.");
 //console.log(nc.partial);
--- cli.js
+++ cli.js
@@ -1,8 +1,119 @@
 var BasicCard = require('./BasicCard'); 
 var ClozeCard = require('./ClozeCard');
+var inquirer = require('inquirer');
+var fs = require('fs');
+var bbcard = {question: []};
 
-var bc = new BasicCard("What was Willie Mays' nickname?", "The Say Hey Kid");
-console.log("Front of Card: " + bc.front + "\nBack of Card: " + bc.back);
 
-var cc = new ClozeCard("Valentina Tereshkova was the first woman to go into space", "Valentina Tereshkova");
-console.log("Complete Sentence: " + cc.text + "\nPartial Sentence: " + cc.partial + "\nDeleted: " + cc.cloze);
+function createBasicCard(){
+   inquirer.prompt([
+    {
+      type: 'input',
+      name: 'front',
+      message: 'Please enter the front of the flashcard (the question).',      
+    },
+    {
+      type: 'input',
+      name: 'back',
+      message: 'Please enter the back of the flashcard (the answer).',      
+    }     
+  ])
+  .then(function(response) {
+
+      var nbCard = new BasicCard(response.front, response.back);
+      bbcard.question.push(nbCard);
+//      console.log(bbcard.question);
+	 fs.writeFile("flashCards.txt", JSON.stringify(bbcard, null, 2), function(err) {
+	if (err){
+		return console.log(err);
+	 }  else {
+		 menu();
+	}
+  });
+});
+};
+
+function createClozeCard(){
+   inquirer.prompt([
+    {
+      type: 'input',
+      name: 'front',
+      message: 'Please enter the full text of the statement/fact.',      
+    },
+    {
+      type: 'input',
+      name: 'cloze',
+      message: 'Please enter the text to be deleted.',      
+    }     
+  ])
+  .then(function(response) {
+  //   console.log("Front of Card: " + response.front + "\nDeleted Text " + response.cloze);
+     var ncCard = new ClozeCard(response.front, response.cloze);
+  //   console.log("nc.front " +  ncCard.front + "\nnc.cloze " + ncCard.cloze + "\nnc.partial " + ncCard.partial );
+
+     bbcard.question.push(ncCard);
+     fs.writeFile("myflashCards.txt", JSON.stringify(bbcard, null, 2), function(err) {
+	   if (err){
+		return console.log(err);
+	   }  else {
+		menu();
+		}
+  });
+});
+};
+
+
+function readMyFile(){
+//console.log(bbcard.question);
+//console.log(bbcard);
+	fs.readFile("./myflashCards.txt", "utf-8", function(err, data){	
+          var qArray = JSON.parse(data, null, 2);
+     //     console.log(qArray);
+     //     console.log(qArray.question.length);
+          for (var i = 0; i < qArray.question.length; i++){
+          	  if (Object.keys(qArray.question[i])[1] === "back"){
+          		var output = "\nQuestion " + (parseInt(i) +parseInt(1)) + " Front: " + qArray.question[i].front;
+          		output += "\n 	   Back:  "  +  qArray.question[i].back;
+          		console.log(output); 
+          	  } else {
+          	  	  var output = "\nQuestion " + (parseInt(i) +parseInt(1)) + " Text: " + qArray.question[i].text;
+          		  output += "\n 	   Cloze:  "  +  qArray.question[i].cloze;
+          		  output += "\n           Partial: " +  qArray.question[i].partial;
+          		  console.log(output); 
+          	  }
+          }
+	});
+ };	
+
+  
+  function menu(){
+  	 inquirer.prompt([
+    {
+       type: "list",
+      message: "What would you like to do?",
+      choices: ["Create basic flashcard", "Create cloze flashcard",  "See all the flashcards I entered", "exit"],
+      name: "menuSelection"       
+    },
+    ])
+  .then(function(response) {
+//  	console.log("menu response " + response.menuSelection);
+  	switch(response.menuSelection) {
+    case "Create basic flashcard":
+        createBasicCard();
+        break;
+    case "Create cloze flashcard":
+        createClozeCard();
+         break;            
+    case "See all the flashcards I entered":
+         readMyFile();
+        break;
+    case "exit":
+         return;
+        break;
+    default:
+        console.log("I don't understand. Please try again");
+  }  //end of switch
+});
+};
+
+menu();
